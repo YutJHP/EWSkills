a115eaf4ba9c4bb98ff9687442bf61e5203e35db
YutJHP <64915333+YutJHP@users.noreply.github.com>
2025-11-22 12:49:45 -0500

    Add files via upload

---
+++ Embedded C/Bitwise Operations/Problem 02/bit_toggle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int toggleFifthBit(int n) {
+    // Write your code here
+    return n ^ (1 << 5);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d", toggleFifthBit(n));
+    return 0;
+}
